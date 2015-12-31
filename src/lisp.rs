c94c3447f495936187825ca140df4c6b833e97ae
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2015-12-31 23:21:10 +1100

    Make a Val::Intrinsic variant for storing inbuilt functions

--- src/lisp.rs
+++ src/lisp.rs
@@ -3,9 +3,10 @@ use itertools::Itertools;
 use std::collections::HashMap;
 use std::f64::consts;
 use std::fmt;
-use std::ops::*;
 use std::rc::Rc;
 use std::str::FromStr;
+
+pub type Intrinsic = fn(Vec<Val>) -> EvalResult<Val>;
 
 #[derive(Debug, Clone)]
 pub enum Val {
@@ -13,11 +14,17 @@ pub enum Val {
     Number(f64),
     Symbol(String),
     // Callable(Proc),
+    Intrinsic(Intrinsic),
 }
 
 impl<'a> From<&'a str> for Val {
     fn from(x: &'a str) -> Val {
         Val::Symbol(x.to_string())
+    }
+}
+impl From<f64> for Val {
+    fn from(x: f64) -> Val {
+        Val::Number(x)
     }
 }
 
@@ -37,61 +44,11 @@ impl Val {
         }
     }
 
-    fn gt(a: &Val, b: &Val) -> EvalResult<Val> {
-        let a = try!(a.extract_number());
-        let b = try!(b.extract_number());
-
-        Ok(Val::from(a > b))
-    }
-
-    fn lt(a: &Val, b: &Val) -> EvalResult<Val> {
-        let a = try!(a.extract_number());
-        let b = try!(b.extract_number());
-
-        Ok(Val::from(a < b))
-    }
-
-    fn gte(a: &Val, b: &Val) -> EvalResult<Val> {
-        let a = try!(a.extract_number());
-        let b = try!(b.extract_number());
-
-        Ok(Val::from(a >= b))
-    }
-
-    fn lte(a: &Val, b: &Val) -> EvalResult<Val> {
-        let a = try!(a.extract_number());
-        let b = try!(b.extract_number());
-
-        Ok(Val::from(a <= b))
-    }
-
-    fn eq(a: &Val, b: &Val) -> EvalResult<Val> {
-        fn eq_bool(a: &Val, b: &Val) -> EvalResult<bool> {
-            match (a, b) {
-                (&Val::Symbol(ref a), &Val::Symbol(ref b)) => Ok(a == b),
-                (&Val::Number(a), &Val::Number(b)) => Ok(a == b),
-                (&Val::List(ref a), &Val::List(ref b)) if a.len() != b.len() => Ok(false),
-                (&Val::List(ref a), &Val::List(ref b)) => {
-                    <_>::zip(a.iter(), b.iter())
-                        .map(|(a, b)| eq_bool(a, b))
-                        .fold_results(true, |acc, x| acc && x)
-                },
-                (a, b) => Err(format!("incompatible types passed to eq, found {} and {}", a, b))
-            }
-        }
-
-        eq_bool(a, b).map(Val::from)
-    }
-
     fn is_false(&self) -> bool {
         match *self {
             Val::Symbol(ref x) => x == "#f",
             _ => false,
         }
-    }
-
-    fn not(&self) -> Val {
-        Val::from(self.is_false())
     }
 }
 
@@ -101,6 +58,7 @@ impl fmt::Display for Val {
             Val::List(ref xs) => write!(f, "({})", xs.iter().format(" ", |x, f| f(x))),
             Val::Number(ref x) => write!(f, "{}", x),
             Val::Symbol(ref x) => write!(f, "{}", x),
+            Val::Intrinsic(_) => write!(f, "<intrinsic>"),
         }
     }
 }
@@ -154,7 +112,18 @@ impl Env {
 
     pub fn standard() -> Env {
         let mut env = Env::new(None);
-        env.define("pi".to_string(), Val::Number(consts::PI));
+        env.define("pi", Val::Number(consts::PI));
+        env.define("+", Val::Intrinsic(intrinsics::add));
+        env.define("-", Val::Intrinsic(intrinsics::sub));
+        env.define("*", Val::Intrinsic(intrinsics::mul));
+        env.define("/", Val::Intrinsic(intrinsics::div));
+        env.define(">", Val::Intrinsic(intrinsics::gt));
+        env.define("<", Val::Intrinsic(intrinsics::lt));
+        env.define(">=", Val::Intrinsic(intrinsics::ge));
+        env.define("<=", Val::Intrinsic(intrinsics::le));
+        env.define("=", Val::Intrinsic(intrinsics::eq));
+        env.define("not", Val::Intrinsic(intrinsics::not));
+        env.define("list", Val::Intrinsic(intrinsics::list));
         env
     }
 
@@ -164,8 +133,8 @@ impl Env {
         })
     }
 
-    fn define(&mut self, var_name: String, val: Val) {
-        match self.vars.insert(var_name, val) {
+    fn define<S: Into<String>>(&mut self, var_name: S, val: Val) {
+        match self.vars.insert(var_name.into(), val) {
             Some(var_name) => panic!("can't define variable '{}', already defined in this scope", var_name),
             None => (),
         }
@@ -219,17 +188,22 @@ impl Env {
                                 Ok(Val::from(false))
                             },
                             // otherwise, call procedure
-                            _ => {
-                                let evaluated_args = try! {
+                            symbol => {
+                                if let Some(&Val::Intrinsic(f)) = self.access(symbol) {
                                     args.map(|arg| self.eval(arg))
                                         .fold_results(vec![], |mut acc, x| { acc.push(x); acc })
-                                };
-                                call_proc(&symbol, evaluated_args)
+                                        .and_then(f)
+                                } else {
+                                    Err(format!("the function '{}' was not defined", symbol))
+                                }
                             },
                         }
                     },
                     Some(_) | None => Err("unknown list form".to_string()),
                 }
+            },
+            Val::Intrinsic(f) => {
+                Ok(Val::Intrinsic(f))
             },
         }
     }
@@ -336,69 +310,100 @@ fn atom(token: String) -> Val {
     }
 }
 
-// def eval(x, env=global_env):
-//     "Evaluate an expression in an environment."
-//     if isinstance(x, Symbol):      # variable reference
-//         return env[x]
-//     elif not isinstance(x, List):  # constant literal
-//         return x
-//     elif x[0] == 'quote':          # (quote exp)
-//         (_, exp) = x
-//         return exp
-//     elif x[0] == 'if':             # (if test conseq alt)
-//         (_, test, conseq, alt) = x
-//         exp = (conseq if eval(test, env) else alt)
-//         return eval(exp, env)
-//     elif x[0] == 'define':         # (define var exp)
-//         (_, var, exp) = x
-//         env[var] = eval(exp, env)
-//     else:                          # (proc arg...)
-//         proc = eval(x[0], env)
-//         args = [eval(arg, env) for arg in x[1:]]
-//         return proc(*args)
+mod intrinsics {
+    use itertools::Itertools;
 
-fn call_proc(proc_name: &str, mut args: Vec<Val>) -> EvalResult<Val> {
-    match proc_name.trim() {
-        "+" => apply_arithmetic(args, f64::add),
-        "-" => apply_arithmetic(args, f64::sub),
-        "*" => apply_arithmetic(args, f64::mul),
-        "/" => apply_arithmetic(args, f64::div),
-        ">" => apply2(args, Val::gt),
-        "<" => apply2(args, Val::lt),
-        ">=" => apply2(args, Val::gte),
-        "<=" => apply2(args, Val::lte),
-        "=" => apply2(args, Val::eq),
-        "not" => apply1(args, |x| Ok(x.not())),
-        "list" => Ok(Val::List(args)),
-        "begin" => {
-            match args.pop() {
-                Some(x) => Ok(x),
-                None => Ok(Val::from(false)),
+    use lisp::{EvalResult, Val};
+
+    use std::ops::*;
+
+    fn foldf64<F: Fn(f64, f64) -> f64>(args: Vec<Val>, acc: f64, f: F) -> EvalResult<Val> {
+        args.iter()
+            .map(Val::extract_number)
+            .fold_results(acc, f)
+            .map(Val::from)
+    }
+
+    fn apply1<F: Fn(&Val) -> EvalResult<Val>>(args: Vec<Val>, f: F) -> EvalResult<Val> {
+        if args.len() != 1 {
+            Err(format!("incorrect number of args for func, expected 1, got {}", args.len()))
+        } else {
+            f(&args[0])
+        }
+    }
+
+    fn apply2<F: Fn(&Val, &Val) -> EvalResult<Val>>(args: Vec<Val>, f: F) -> EvalResult<Val> {
+        if args.len() != 2 {
+            Err(format!("incorrect number of args for func, expected 2, got {}", args.len()))
+        } else {
+            f(&args[0], &args[1])
+        }
+    }
+
+    fn apply2f64<F: Fn(f64, f64) -> EvalResult<Val>>(args: Vec<Val>, f: F) -> EvalResult<Val> {
+        if args.len() != 2 {
+            Err(format!("incorrect number of args for func, expected 2, got {}", args.len()))
+        } else {
+            f(try!(args[0].extract_number()),
+              try!(args[1].extract_number()))
+        }
+    }
+
+    pub fn add(args: Vec<Val>) -> EvalResult<Val> {
+        foldf64(args, 0.0, f64::add)
+    }
+
+    pub fn sub(args: Vec<Val>) -> EvalResult<Val> {
+        foldf64(args, 0.0, f64::sub)
+    }
+
+    pub fn mul(args: Vec<Val>) -> EvalResult<Val> {
+        foldf64(args, 1.0, f64::mul)
+    }
+
+    pub fn div(args: Vec<Val>) -> EvalResult<Val> {
+        foldf64(args, 1.0, f64::div)
+    }
+
+    pub fn gt(args: Vec<Val>) -> EvalResult<Val> {
+        apply2f64(args, |a, b| Ok(Val::from(a > b)))
+    }
+
+    pub fn lt(args: Vec<Val>) -> EvalResult<Val> {
+        apply2f64(args, |a, b| Ok(Val::from(a < b)))
+    }
+
+    pub fn ge(args: Vec<Val>) -> EvalResult<Val> {
+        apply2f64(args, |a, b| Ok(Val::from(a >= b)))
+    }
+
+    pub fn le(args: Vec<Val>) -> EvalResult<Val> {
+        apply2f64(args, |a, b| Ok(Val::from(a <= b)))
+    }
+
+    pub fn eq(args: Vec<Val>) -> EvalResult<Val> {
+        fn eq_bool(a: &Val, b: &Val) -> EvalResult<bool> {
+            match (a, b) {
+                (&Val::Symbol(ref a), &Val::Symbol(ref b)) => Ok(a == b),
+                (&Val::Number(a), &Val::Number(b)) => Ok(a == b),
+                (&Val::List(ref a), &Val::List(ref b)) if a.len() != b.len() => Ok(false),
+                (&Val::List(ref a), &Val::List(ref b)) => {
+                    <_>::zip(a.iter(), b.iter())
+                        .map(|(a, b)| eq_bool(a, b))
+                        .fold_results(true, |acc, x| acc && x)
+                },
+                (a, b) => Err(format!("incompatible types passed to eq, found {} and {}", a, b))
             }
-        },
-        _ => Err(format!("unknown proc '{}'", proc_name)),
+        }
+
+        apply2(args, |a, b| eq_bool(a, b).map(Val::from))
     }
-}
 
-fn apply_arithmetic<F: Fn(f64, f64) -> f64>(args: Vec<Val>, operator: F) -> EvalResult<Val> {
-    args.iter()
-        .map(Val::extract_number)
-        .fold_results(0.0, operator)
-        .map(Val::Number)
-}
-
-fn apply1<F: Fn(&Val) -> EvalResult<Val>>(args: Vec<Val>, func: F) -> EvalResult<Val> {
-    if args.len() != 1 {
-        Err(format!("incorrect number of args for func, expected 1, got {}", args.len()))
-    } else {
-        func(&args[0])
+    pub fn not(args: Vec<Val>) -> EvalResult<Val> {
+        apply1(args, |x| Ok(Val::from(x.is_false())))
     }
-}
 
-fn apply2<F: Fn(&Val, &Val) -> EvalResult<Val>>(args: Vec<Val>, func: F) -> EvalResult<Val> {
-    if args.len() != 2 {
-        Err(format!("incorrect number of args for func, expected 2, got {}", args.len()))
-    } else {
-        func(&args[0], &args[1])
+    pub fn list(args: Vec<Val>) -> EvalResult<Val> {
+        Ok(Val::List(args))
     }
 }
